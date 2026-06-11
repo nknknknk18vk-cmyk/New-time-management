d4e82ee388c7c76c20aaf3368259fd2f6519a13b
nknknknk18vk-cmyk <nknknknk18vk@gmail.com>
2026-06-11 23:39:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const cacheName = 'studysync-v7-cache';
+const cacheName = 'studysync-v8'; // Changed the name to force an update
 const appFiles = [
   './index.html',
   './manifest.json'
 ];
 
-// Install the service worker and cache the files
+// Install and cache files
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Forces the new service worker to activate immediately
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
       return cache.addAll(appFiles);
@@ -13,12 +14,34 @@ self.addEventListener('install', (e) => {
   );
 });
 
-// Serve cached files when offline
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName) {
+          return caches.delete(key); // Deletes the old broken cache
+        }
+      }));
     })
   );
 });
 
+// Network-First Strategy
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        // If we get a valid response from the internet, update the cache
+        if (response && response.status === 200) {
+            let responseClone = response.clone();
+            caches.open(cacheName).then((cache) => cache.put(e.request, responseClone));
+        }
+        return response;
+      })
+      .catch(() => {
+        // If the internet is down, fall back to the cache
+        return caches.match(e.request);
+      })
+  );
+});
